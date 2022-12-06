9dff5969980cbe3996a9724e9a07e4dedd3d6901
weliton <welitoncpb@gmail.com>
2022-12-06 15:48:46 -0300

    Inserido o formulário do rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
   def index
+    @all_ratings = Movie.all_ratings
     @movies = Movie.all.order(params[:sort_by])
   end
 
